10a45757f393f7c22be6cfa89b3fa08fa1f109b9
Arimahendra1806 <arimahendra1806@gmail.com>
2022-10-19 01:04:56 +0700

    setup authentication

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\CheckRole;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +19,41 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
+});
+
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+/* Login */
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'postLogin'])->name('post.login');
+
+/* Register */
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/register', [AuthController::class, 'postRegister'])->name('post.login');
+
+/* Logout */
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+/* Middleware && Auth */
+Route::group(['middleware' => 'auth'], function(){
+    /* Admin */
+    Route::group(['middleware' => 'CheckRole:Admin'], function(){
+
+    });
+    /* Customer */
+    Route::group(['middleware' => 'CheckRole:Customer'], function(){
+
+    });
+    /* Owner */
+    Route::group(['middleware' => 'CheckRole:Owner'], function(){
+
+    });
+
+    /* Owner & Admin */
+    Route::group(['middleware' => 'CheckRole:Owner,Admin'], function(){
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    });
 });
